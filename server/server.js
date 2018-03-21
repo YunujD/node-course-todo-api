a920398460f8919ade1daff677d971de40245a2e
YunujD <yunuj14@gmail.com>
2018-03-21 23:29:55 +0545

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -30,6 +31,25 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    console.log('Id is not valid');
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }, (err) => {
+    if (err) {
+      res.status(400).send({});
+    }
+  });
+
 });
 
 app.listen(3000, () => {
